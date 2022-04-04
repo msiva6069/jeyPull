75787ab4c68d93affd4ebe7ad5244997bf995c0d
msiva6069 <msiva6069@gmail.com>
2022-04-04 22:27:54 +0530

    Added siva code

---
+++ src/test/java/org/hcl/GitPull.java
@@ -0,0 +1,31 @@
+package org.hcl;
+
+public class GitPull {
+	
+	public void method1() {
+		System.out.println("method 1");
+		}
+	
+	public void method2() {
+		System.out.println("method 2");
+		}
+	public void method3() {
+		System.out.println("method 3");
+		}
+	public void method4() {
+		System.out.println("method 4");
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
