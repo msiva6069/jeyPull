b031f067feeb54d2cf58bd15cd1f52dbceb01cb7
msiva6069 <msiva6069@gmail.com>
2022-04-04 23:19:50 +0530

    jeyjra code

--- src/test/java/org/hcl/GitPull.java
+++ src/test/java/org/hcl/GitPull.java
@@ -16,6 +16,21 @@ public class GitPull {
 		System.out.println("method 4");
 		}
 	
+	public void method9() {
+		System.out.println("method 9");
+		}
+	
+	public void method10() {
+		System.out.println("method 10");
+		}
+	
+	public void method11() {
+		System.out.println("method 11");
+		}
+	
+	public void method12() {
+		System.out.println("method 12");
+		}
 	
 	
 	
